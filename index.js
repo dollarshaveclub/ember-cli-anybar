ef03a9a4ab6dd2febb2cfbee20ad1cc921a30a1b
Stefanie Koenig <stefanie.koenig@dollarshaveclub.com>
2016-12-27 21:30:51 -0800

    add anybar alert colors to index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* jshint node: true */
 'use strict';
 
+const anybar = require('anybar');
+
 module.exports = {
-  name: 'ember-cli-anybar'
+  name: 'ember-cli-anybar',
+
+  preBuild: function() {
+    anybar('yellow');
+  },
+
+  buildError: function() {
+    anybar('red');
+  },
+
+  postBuild: function() {
+    anybar('green');
+  },
+
 };
